14b287ecd4dbf662cfa050a359457145f47a1984
cmfatih <cmuratfatih@gmail.com>
2014-04-13 20:31:58 -0400

    Improve logic

--- test/test-all.js
+++ test/test-all.js
@@ -13,6 +13,19 @@ var gTestList = {
 // Tests
 console.log('test-all.js');
 
+// Loads the database and search ip addresses.
+function loadAndSearch() {
+  console.log('dbLoad: ' + mIP2CO.dbLoad());
+
+  var ipS = mIP2CO.ipSearch(['74.125.225.71', '98.138.253.109']);
+  if(!ipS.error) {
+    console.log('ipSearch: ' + ipS.timeE + 'ms');
+    console.log(ipS);
+  } else {
+    console.log('ipSearch: ' + ipS.error);
+  }
+}
+
 // Test for mics. features
 if(gTestList.MISC === true) {
   console.log('dataDirCheck: ' + mIP2CO.dataDirCheck());
@@ -24,18 +37,11 @@ if(gTestList.MISC === true) {
   if(mIP2CO.dbCSVCheckExp(48)) {
     mIP2CO.dbGet().then(function() {
       console.log('dbGet: DONE!');
+      loadAndSearch();
     }, function(err) {
       console.log('dbGet: ' + err);
     });
-  }
-
-  console.log('dbLoad: ' + mIP2CO.dbLoad());
-
-  var ipS = mIP2CO.ipSearch(['74.125.225.71', '98.138.253.109']);
-  if(!ipS.error) {
-    console.log('ipSearch: DONE! (' + ipS.timeE + 'ms)');
-    console.log(ipS);
   } else {
-    console.log('ipSearch: ' + ipS.error);
-  };
+    loadAndSearch();
+  }
 }
